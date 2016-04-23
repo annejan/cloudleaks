1c475dc0d432a2fd29d87d33297a4aa1081afbc3
Anne Jan Brouwer <brouwer@annejan.com>
2016-04-23 20:44:40 +0200

    Unlock

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,11 +12,18 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
+        if (env('DB_CONNECTION') != 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        }
         DB::table('users')->truncate();
         User::create([
             'name' => 'Anne Jan',
             'email' => 'brouwer@annejan.com',
             'password' => Hash::make('mo6tah8I')
         ]);
+        if (env('DB_CONNECTION') != 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        }
     }
 }
