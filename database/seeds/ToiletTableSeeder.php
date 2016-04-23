c44fd2135f93866c2dba3b0d9b783995bb04f9a8
Anne Jan Brouwer <brouwer@annejan.com>
2016-04-23 20:57:06 +0200

    Add my first toilet

---
+++ database/seeds/ToiletTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Toilet;
+use Illuminate\Database\Eloquent\Model;
+
+class ToiletTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+        if (env('DB_CONNECTION') != 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        }
+        DB::table('toilets')->truncate();
+        Toilet::create([
+            'name' => 'Ons eerste toilet',
+            'apikey' => 'jaeceifaexoonahsohgheCheiludaSeebaey2IiZaeCh1Zaph0aiveec4hohph3j'
+        ]);
+        if (env('DB_CONNECTION') != 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        }
+    }
+}
