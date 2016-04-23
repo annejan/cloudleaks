e7910e376123c4480fc7c69800d0280f5242251a
Anne Jan Brouwer <brouwer@annejan.com>
2016-04-23 20:26:04 +0200

    Migration

---
+++ database/migrations/2016_04_23_202946_create_samples_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSamplesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('samples', function(Blueprint $table)
+		{
+			$table->increments('id');
+			
+                        $table->integer('profile_id')->unsigned();
+19	                $table->foreign('profile_id')->references('id')->on('profiles');
+                        $table->integer('toilet_id')->unsigned();
+19	                $table->foreign('toilet_id')->references('id')->on('toilets');		
+
+                        $table->integer('dutation')->unsigned();
+                        $table->integer('volume')->unsigned();
+                        $table->integer('hue')->unsigned();
+                        $table->integer('saturation')->unsigned();
+                        $table->integer('brightness')->unsigned();
+                        $table->integer('temperature')->unsigned();
+                        $table->integer('acidity')->unsigned();
+                        $table->integer('glucose')->unsigned();
+                        $table->boolean('pregnant');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('samples');
+	}
+
+}
